6f5fd509f78003edc8cb0beee4da7aeaec31e677
yoke.yue <yoke.yuer@gmail.com>
2026-02-17 21:52:14 +0400

    add

--- home/linux/gui/base/browsers.nix
+++ home/linux/gui/base/browsers.nix
@@ -5,9 +5,13 @@
 {
   home.packages = with pkgs; [
     nixpaks.firefox # sandboxed via nixpak (see hardening/nixpaks/)
+    # Install Firefox icon (nixpak sandbox doesn't expose the original icon)
+    (pkgs.runCommand "firefox-icons" {} ''
+      mkdir -p $out/share
+      ln -s ${pkgs.firefox}/share/icons $out/share/icons
+    '')
   ];
 
-  # source code: https://github.com/nix-community/home-manager/blob/master/modules/programs/chromium.nix
   programs.google-chrome = {
     enable = true;
     package = if pkgs.stdenv.hostPlatform.isAarch64 then pkgs.chromium else pkgs.google-chrome;
