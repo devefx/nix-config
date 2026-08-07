47b57a620078b16764f9fd96eb1dc8e038d6690a
Yoke <yoke.yuer@gmail.com>
2026-08-07 10:22:40 +0400

    chore: add sjtu mirror and cache.nixos.org to substituters

--- modules/base/nix.nix
+++ modules/base/nix.nix
@@ -24,9 +24,11 @@
     trusted-users = [ myvars.username ];
 
     substituters = [
-      "https://mirrors.ustc.edu.cn/nix-channels/store"
+      "https://mirror.sjtu.edu.cn/nix-channels/store"
       "https://mirrors.tuna.tsinghua.edu.cn/nix-channels/store"
+      "https://mirrors.ustc.edu.cn/nix-channels/store"
       "https://nix-community.cachix.org"
+      "https://cache.nixos.org"
     ];
 
     trusted-public-keys = [
